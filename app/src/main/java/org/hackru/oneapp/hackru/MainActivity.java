fe6c5ae96d41c8df437544e460b2618b64df4da8
Rishab Ravikumar <rishabravikumar@yahoo.com>
2018-03-13 16:56:49 -0400

    MapFragment Works !

--- app/src/main/java/org/hackru/oneapp/hackru/MainActivity.java
+++ app/src/main/java/org/hackru/oneapp/hackru/MainActivity.java
@@ -1,4 +1,4 @@
-package org.hackru.oneapp.hackru;
+package com.example.rishabravikumar.hackru;
 
 import android.Manifest;
 import android.content.Intent;
@@ -10,6 +10,7 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -19,116 +20,114 @@ import android.view.View;
 public class MainActivity extends AppCompatActivity {
     String TAG = "";
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-            // END BOILERPLATE CODE
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // END BOILERPLATE CODE
 
-            if(true) { // If the user isn't logged in, make them log in
-                Intent mainActivityIntent = new Intent(this, LoginActivity.class);
-                startActivity(mainActivityIntent);
-            }
-
-            // TODO: FIX BUG WHERE APP CRASHES FROM INFINITE LOOP IF LOCATION PERMISSION IS DENIED
-            if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
-                while(true) {
-                    if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                        break;
-                    }
-                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
-                }
-            }
-
-            final FragmentManager fragmentManager = getSupportFragmentManager();
-            fragmentManager.beginTransaction().add(R.id.content_frame, new TimerFragment(), "timer").commit(); // Adds the timer fragment when the app launches so it's displaying
-
-            final BottomNavigationView bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottom_navigation);
-            bottomNavigationView.getMenu().findItem(R.id.menu_timer).setChecked(true); // Makes the timer menu item checked on app load since it's the first item that appears (not sure if this is necessary)
-            //TODO: RECREATE FRAGMENT IF USER CLICKS MENU ITEM THAT IS ALREADY SELECTED
-            bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-                @Override
-                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                    bottomNavigationView.getMenu().findItem(R.id.menu_announcements).setChecked(false);
-                    bottomNavigationView.getMenu().findItem(R.id.menu_event).setChecked(false);
-                    bottomNavigationView.getMenu().findItem(R.id.menu_map).setChecked(false);
-                    bottomNavigationView.getMenu().findItem(R.id.menu_timer).setChecked(false);
-                    item.setChecked(true);
-
-                    if(item.getItemId() == R.id.menu_timer) {
-
-                        if(fragmentManager.findFragmentByTag("timer")!=null) {
-                            // if the fragment exists, show it
-                            fragmentManager.beginTransaction().show(fragmentManager.findFragmentByTag("timer")).commit();
-                        } else {
-                            // if the fragment does not exist yet, add it to the fragment manager
-                            fragmentManager.beginTransaction().add(R.id.content_frame, new TimerFragment(), "timer").commit();
-                        }
-
-                        if(fragmentManager.findFragmentByTag("map") !=null) {fragmentManager.beginTransaction().hide(fragmentManager.findFragmentByTag("map")).commit();}
-                        if(fragmentManager.findFragmentByTag("events") !=null) {fragmentManager.beginTransaction().hide(fragmentManager.findFragmentByTag("events")).commit();}
-                        if(fragmentManager.findFragmentByTag("announcements") !=null) {fragmentManager.beginTransaction().hide(fragmentManager.findFragmentByTag("announcements")).commit();}
-
-                        return true;
-                    } else if(item.getItemId() == R.id.menu_announcements) {
-
-                        if(fragmentManager.findFragmentByTag("announcements")!=null) {
-                            // if the fragment exists, show it
-                            fragmentManager.beginTransaction().show(fragmentManager.findFragmentByTag("announcements")).commit();
-                        } else {
-                            // if the fragment does not exist yet, add it to the fragment manager
-                            fragmentManager.beginTransaction().add(R.id.content_frame, new AnnouncementsFragment(), "announcements").commit();
-                        }
-
-                        if(fragmentManager.findFragmentByTag("map") !=null) {fragmentManager.beginTransaction().hide(fragmentManager.findFragmentByTag("map")).commit();}
-                        if(fragmentManager.findFragmentByTag("events") !=null) {fragmentManager.beginTransaction().hide(fragmentManager.findFragmentByTag("events")).commit();}
-                        if(fragmentManager.findFragmentByTag("timer") !=null) {fragmentManager.beginTransaction().hide(fragmentManager.findFragmentByTag("timer")).commit();}
-
-                        return true;
-                    } else if(item.getItemId() == R.id.menu_event) {
-                        if(fragmentManager.findFragmentByTag("events")!=null) {
-                            // if the fragment exists, show it
-                            fragmentManager.beginTransaction().show(fragmentManager.findFragmentByTag("events")).commit();
-                        } else {
-                            // if the fragment does not exist yet, add it to the fragment manager
-                            fragmentManager.beginTransaction().add(R.id.content_frame, new EventsFragment(), "events").commit();
-                        }
-
-                        if(fragmentManager.findFragmentByTag("map") !=null) {fragmentManager.beginTransaction().hide(fragmentManager.findFragmentByTag("map")).commit();}
-                        if(fragmentManager.findFragmentByTag("timer") !=null) {fragmentManager.beginTransaction().hide(fragmentManager.findFragmentByTag("timer")).commit();}
-                        if(fragmentManager.findFragmentByTag("announcements") !=null) {fragmentManager.beginTransaction().hide(fragmentManager.findFragmentByTag("announcements")).commit();}
-
-                        return true;
-                    } else if(item.getItemId() == R.id.menu_map) {
-                        //TODO: GET MAP FRAGMENT WORKING (SEAN CAN'T FIGURE OUT WHY IT ISN'T ANYMORE)
-                        if(fragmentManager.findFragmentByTag("map")!=null) {
-                            // if the fragment exists, show it
-                            fragmentManager.beginTransaction().show(fragmentManager.findFragmentByTag("map")).commit();
-                        } else {
-                            // if the fragment does not exist yet, add it to the fragment manager
-                            fragmentManager.beginTransaction().add(R.id.content_frame, new TimerFragment(), "map").commit();
-                        }
-
-                        if(fragmentManager.findFragmentByTag("events") !=null) {fragmentManager.beginTransaction().hide(fragmentManager.findFragmentByTag("events")).commit();}
-                        if(fragmentManager.findFragmentByTag("timer") !=null) {fragmentManager.beginTransaction().hide(fragmentManager.findFragmentByTag("timer")).commit();}
-                        if(fragmentManager.findFragmentByTag("announcements") !=null) {fragmentManager.beginTransaction().hide(fragmentManager.findFragmentByTag("announcements")).commit();}
-
-                        return true;
-                    }
-
-                    return false;
-                }
-            });
-
-            FloatingActionButton myFab = (FloatingActionButton) findViewById(R.id.qr_fab);
-            myFab.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View v) {
-                    Log.e("FAB: ", "Clicked");
-                    QRDialogueFragment QRFragment = new QRDialogueFragment();
-                    QRFragment.show(fragmentManager, "fragment_qrdialogue");
-                }
-            });
-
+        if(true) { // If the user isn't logged in, make them log in
+            Intent mainActivityIntent = new Intent(this, LoginActivity.class);
+            startActivity(mainActivityIntent);
         }
+
+        // TODO: FIX BUG WHERE APP CRASHES FROM INFINITE LOOP IF LOCATION PERMISSION IS DENIED
+        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+            while(true) {
+                if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    break;
+                }
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+            }
+        }
+
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().add(R.id.content_frame, new TimerFragment(), "timer").commit(); // Adds the timer fragment when the app launches so it's displaying
+
+        final BottomNavigationView bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottom_navigation);
+        bottomNavigationView.getMenu().findItem(R.id.menu_timer).setChecked(true); // Makes the timer menu item checked on app load since it's the first item that appears (not sure if this is necessary)
+        //TODO: RECREATE FRAGMENT IF USER CLICKS MENU ITEM THAT IS ALREADY SELECTED
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                bottomNavigationView.getMenu().findItem(R.id.menu_announcements).setChecked(false);
+                bottomNavigationView.getMenu().findItem(R.id.menu_event).setChecked(false);
+                bottomNavigationView.getMenu().findItem(R.id.menu_map).setChecked(false);
+                bottomNavigationView.getMenu().findItem(R.id.menu_timer).setChecked(false);
+                item.setChecked(true);
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                if(item.getItemId() == R.id.menu_timer) {
+
+                    if(fragmentManager.findFragmentByTag("timer")!=null) {
+                        // if the fragment exists, show it
+                        fragmentTransaction.show(fragmentManager.findFragmentByTag("timer"));
+                    } else {
+                        // if the fragment does not exist yet, add it to the fragment manager
+                        fragmentTransaction.add(R.id.content_frame, new TimerFragment(), "timer");
+                    }
+
+                    if(fragmentManager.findFragmentByTag("map") !=null) {fragmentTransaction.hide(fragmentManager.findFragmentByTag("map"));}
+                    if(fragmentManager.findFragmentByTag("events") !=null) {fragmentTransaction.hide(fragmentManager.findFragmentByTag("events"));}
+                    if(fragmentManager.findFragmentByTag("announcements") !=null) {fragmentTransaction.hide(fragmentManager.findFragmentByTag("announcements"));}
+                    fragmentTransaction.commit();
+                    return true;
+                } else if(item.getItemId() == R.id.menu_announcements) {
+
+                    if(fragmentManager.findFragmentByTag("announcements")!=null) {
+                        // if the fragment exists, show it
+                        fragmentTransaction.show(fragmentManager.findFragmentByTag("announcements"));
+                    } else {
+                        // if the fragment does not exist yet, add it to the fragment manager
+                        fragmentTransaction.add(R.id.content_frame, new AnnouncementsFragment(), "announcements");
+                    }
+
+                    if(fragmentManager.findFragmentByTag("map") !=null) {fragmentTransaction.hide(fragmentManager.findFragmentByTag("map"));}
+                    if(fragmentManager.findFragmentByTag("events") !=null) {fragmentTransaction.hide(fragmentManager.findFragmentByTag("events"));}
+                    if(fragmentManager.findFragmentByTag("timer") !=null) {fragmentTransaction.hide(fragmentManager.findFragmentByTag("timer"));}
+                    fragmentTransaction.commit();
+                    return true;
+                } else if(item.getItemId() == R.id.menu_event) {
+                    if(fragmentManager.findFragmentByTag("events")!=null) {
+                        // if the fragment exists, show it
+                        fragmentTransaction.show(fragmentManager.findFragmentByTag("events"));
+                    } else {
+                        // if the fragment does not exist yet, add it to the fragment manager
+                        fragmentTransaction.add(R.id.content_frame, new EventsFragment(), "events");
+                    }
+
+                    if(fragmentManager.findFragmentByTag("map") !=null) {fragmentTransaction.hide(fragmentManager.findFragmentByTag("map"));}
+                    if(fragmentManager.findFragmentByTag("timer") !=null) {fragmentTransaction.hide(fragmentManager.findFragmentByTag("timer"));}
+                    if(fragmentManager.findFragmentByTag("announcements") !=null) {fragmentTransaction.hide(fragmentManager.findFragmentByTag("announcements"));}
+                    fragmentTransaction.commit();
+                    return true;
+                } else if(item.getItemId() == R.id.menu_map) {
+                    //TODO: GET MAP FRAGMENT WORKING ---- IT WORKS NOW !!
+                    if(fragmentManager.findFragmentByTag("map")!=null) {
+                        // if the fragment exists, show it
+                        fragmentTransaction.show(fragmentManager.findFragmentByTag("map"));
+                    } else {
+                        // if the fragment does not exist yet, add it to the fragment manager
+                        fragmentTransaction.add(R.id.content_frame, new MapFragment(), "map");
+                    }
+
+                    if(fragmentManager.findFragmentByTag("events") !=null) {fragmentTransaction.hide(fragmentManager.findFragmentByTag("events"));}
+                    if(fragmentManager.findFragmentByTag("timer") !=null) {fragmentTransaction.hide(fragmentManager.findFragmentByTag("timer"));}
+                    if(fragmentManager.findFragmentByTag("announcements") !=null) {fragmentTransaction.hide(fragmentManager.findFragmentByTag("announcements"));}
+                    fragmentTransaction.commit();
+                    return true;
+                }
+
+                return false;
+            }
+        });
+        FloatingActionButton myFab = (FloatingActionButton) findViewById(R.id.qr_fab);
+        myFab.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.e("FAB: ", "Clicked");
+                QRDialogueFragment QRFragment = new QRDialogueFragment();
+                QRFragment.show(fragmentManager, "fragment_qrdialogue");
+            }
+        });
     }
+}
