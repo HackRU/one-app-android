7d9e6409f97d8004f540ee6cbed8236a36785bd4
SeanTaylorLane <seantlane1999@gmail.com>
2018-07-22 19:48:31 -0400

    Implement navigation drawer

--- app/src/main/java/org/hackru/oneapp/hackru/MainActivity.kt
+++ app/src/main/java/org/hackru/oneapp/hackru/MainActivity.kt
@@ -1,7 +1,11 @@
 package org.hackru.oneapp.hackru
 
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +16,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setUpActionBar()
+
         // Set Timer as the default tab when the app is opened for the first time in a while or was
         // terminated manually by the user
         if (savedInstanceState == null) {
@@ -19,25 +25,48 @@ class MainActivity : AppCompatActivity() {
             switchFragment(TimerFragment.newInstance())
         }
         bottom_navigation.setOnNavigationItemSelectedListener {
-            when(it.itemId) {
+            return@setOnNavigationItemSelectedListener when(it.itemId) {
                 R.id.bottom_navigation_announcements -> {
                     // User clicked on Announcements
                     switchFragment(AnnouncementsFragment.newInstance())
-                    return@setOnNavigationItemSelectedListener true
+                    true
                 }
                 R.id.bottom_navigation_timer -> {
                     // User clicked on Timer
                     switchFragment(TimerFragment.newInstance())
-                    return@setOnNavigationItemSelectedListener true
+                    true
                 }
                 R.id.bottom_navigation_events -> {
                     // User clicked on Events
                     switchFragment(EventsFragment.newInstance())
-                    return@setOnNavigationItemSelectedListener true
+                   true
+                }
+                else -> false
+            }
+        }
+
+        drawer_navigation.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.drawer_map -> {
+
+                }
+                R.id.drawer_scanner -> {
+
+                }
+                R.id.drawer_settings -> {
+
+                }
+                R.id.drawer_feedback -> {
+
+                }
+                R.id.drawer_about -> {
+
                 }
             }
-            return@setOnNavigationItemSelectedListener false
+            drawer_layout.closeDrawer(GravityCompat.START)
+            true
         }
+
     }
 
     /**
@@ -51,5 +80,21 @@ class MainActivity : AppCompatActivity() {
         supportFragmentManager.beginTransaction()
                 .replace(R.id.fragment_container, fragment)
                 .commit()
+    }
+
+    private fun setUpActionBar() {
+        setSupportActionBar(toolbar)
+        val drawerToggle = ActionBarDrawerToggle(
+                this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer_layout.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+    }
+
+    override fun onBackPressed() {
+        if(drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 }
