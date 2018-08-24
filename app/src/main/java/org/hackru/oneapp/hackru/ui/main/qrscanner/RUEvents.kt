268e0b08ab8e72f9f30b57c2a9033c58f9504e29
user <user1@users-MacBook-Pro.local>
2018-08-24 12:33:17 -0400

    remove type dec

--- app/src/main/java/org/hackru/oneapp/hackru/ui/main/qrscanner/RUEvents.kt
+++ app/src/main/java/org/hackru/oneapp/hackru/ui/main/qrscanner/RUEvents.kt
@@ -3,11 +3,11 @@ package com.ridhwaan.ruscanner
 
 class RUEvents {
     companion object  {
-        val BREAKFAST:Int = 0
-        val LUNCH:Int = 1
-        val DINNER:Int = 2
-        val MIDNIGHT_SNACK:Int = 3
-        val RANDOM_EVENT:Int = 4
+        val BREAKFAST = 0
+        val LUNCH = 1
+        val DINNER = 2
+        val MIDNIGHT_SNACK = 3
+        val RANDOM_EVENT = 4
 
     }
 
