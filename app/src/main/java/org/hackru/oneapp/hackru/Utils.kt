017a9f2f85b9627eb01fb9821aeaa0833bb6b388
SeanTaylorLane <seantlane1999@gmail.com>
2018-09-24 17:09:10 -0400

    SharedPreferences for account management

--- app/src/main/java/org/hackru/oneapp/hackru/Utils.kt
+++ app/src/main/java/org/hackru/oneapp/hackru/Utils.kt
@@ -1,9 +1,56 @@
 package org.hackru.oneapp.hackru
 
 import android.content.Context
+import android.preference.PreferenceManager
 
 object Utils {
         fun convertDpToPx(context: Context, dp: Float): Float {
             return dp * context.resources.displayMetrics.density
         }
+
+    // TODO: Make this a singleton injected with Dagger 2
+    object SharedPreferences {
+        val KEY_AUTH_TOKEN = "authtoken"
+        val KEY_EMAIL = "email"
+        val KEY_CAN_SCAN = "can_scan"
+        // TODO: Set up scanner for allowing waitlist
+        val KEY_ALLOW_WAITLIST = "allow_only_accepted"
+
+        fun setAuthToken(context: Context, authToken: String) {
+            PreferenceManager.getDefaultSharedPreferences(context)
+                    .edit()
+                    .putString(KEY_AUTH_TOKEN, authToken)
+                    .commit()
+        }
+
+        fun getAuthToken(context: Context): String? {
+            return PreferenceManager.getDefaultSharedPreferences(context)
+                    .getString(KEY_AUTH_TOKEN, "")
+        }
+
+        fun setEmail(context: Context, email: String) {
+            PreferenceManager.getDefaultSharedPreferences(context)
+                    .edit()
+                    .putString(KEY_EMAIL, email)
+                    .apply()
+        }
+
+        fun getEmail(context: Context): String? {
+            return PreferenceManager.getDefaultSharedPreferences(context)
+                    .getString(KEY_EMAIL, null)
+        }
+
+        fun setCanScan(context: Context, canScan: Boolean) {
+            PreferenceManager.getDefaultSharedPreferences(context)
+                    .edit()
+                    .putBoolean(KEY_CAN_SCAN, canScan)
+                    .apply()
+        }
+
+        fun getCanScan(context: Context): Boolean {
+            return PreferenceManager.getDefaultSharedPreferences(context)
+                    .getBoolean(KEY_CAN_SCAN, false)
+        }
+
+    }
 }
