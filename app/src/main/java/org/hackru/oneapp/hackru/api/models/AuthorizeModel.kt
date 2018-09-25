f044b4f0562e04c11492aabfa2e6ccf4ecb9a4be
SeanTaylorLane <seantlane1999@gmail.com>
2018-09-25 00:56:08 -0400

    Create authorize endpoint with model

---
+++ app/src/main/java/org/hackru/oneapp/hackru/api/models/AuthorizeModel.kt
@@ -0,0 +1,8 @@
+package org.hackru.oneapp.hackru.api.models
+
+object AuthorizeModel {
+
+    data class Response(val statusCode: String,
+                        val body: String)
+
+}
--- app/src/main/java/org/hackru/oneapp/hackru/api/services/LcsService.kt
+++ app/src/main/java/org/hackru/oneapp/hackru/api/services/LcsService.kt
@@ -1,11 +1,16 @@
 package org.hackru.oneapp.hackru.api.services
 
 import org.hackru.oneapp.hackru.api.models.AnnouncementsModel
+import org.hackru.oneapp.hackru.api.models.AuthorizeModel
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface LcsService {
 
     @GET("dayof-slack")
     fun getAnnouncements(): Call<AnnouncementsModel.Response>
+
+    @POST("authorize")
+    fun authorize(): Call<AuthorizeModel.Response>
 }
