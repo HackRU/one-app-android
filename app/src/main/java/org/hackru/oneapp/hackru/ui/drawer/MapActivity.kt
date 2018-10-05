a7c169355eb602c6dbd7b56233b9fe70bed451cd
SeanTaylorLane <seantlane1999@gmail.com>
2018-10-05 03:12:10 -0400

    Implement map loading with Glide

---
+++ app/src/main/java/org/hackru/oneapp/hackru/ui/drawer/MapActivity.kt
@@ -0,0 +1,12 @@
+package org.hackru.oneapp.hackru.ui.drawer
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MapActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+    }
+}
